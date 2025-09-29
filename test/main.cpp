7fdc0b448965082916050dae6550ceb6c986c1fd
tazy <gardendistrict0x0@outlook.com>
2025-09-29 20:54:06 +1000

    [ fix ] Adapt test file to new namespaces

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,7 @@
 #include "../src/Display.h"
 
 int main() {
-    ch_window win("hello", 640, 480, true);
+    chai::internal::ch_window win("hello", 640, 480, true);
     win.dbg_out("This is a debug message from main.cpp");
     win.set_icon("C:/Programs/chai-ui/test/icon.ico");
 }
