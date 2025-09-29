1cf8d23a89b47541247ebe511eb43d0575230d88
tazy <gardendistrict0x0@outlook.com>
2025-09-30 09:45:11 +1000

    Delete tests

--- test/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "../src/Display.h"
-
-int main() {
-    chai::internal::ch_window win("hello", 640, 480, true);
-    win.dbg_out("This is a debug message from main.cpp");
-    win.set_icon("C:/Programs/chai-ui/test/icon.ico");
-}
