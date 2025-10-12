4745df5131cbac85446b7466a302389d1216de35
Garden <gardendistrict0x0@outlook.com>
2025-10-12 14:44:34 +1100

    [ bug ] Change namspace capitalisation in example

--- example/main.cpp
+++ example/main.cpp
@@ -1,7 +1,7 @@
 #include "include/Chai/Inter/Display.hpp"
 
 int main(void) {
-    chai::internal::ch_window win("Hello my fellas", 800, 600, DBG_ON);
+    Chai::Internal::ch_window win("Hello my fellas", 800, 600, DBG_ON);
 
     win.create();
     win.run();
