51a741d2cb4dc46484d37929078c9889cc46fa98
Garden <gardendistrict0x0@outlook.com>
2025-10-22 08:36:22 +1100

    [ ex ] Add new features to example

--- example/main.cpp
+++ example/main.cpp
@@ -2,6 +2,7 @@
 
 int main(void) {
     Chai::Internal::ch_window win("Test window", 800, 600, DBG_ON);
-    
+    Chai::Internal::ch_rect rectangle(100, 100, 300, 300, D2D1::ColorF(D2D1::ColorF::Blue));
+    win.draw(std::make_unique<Chai::Internal::ch_rect>(rectangle));
     win.run();
 }
