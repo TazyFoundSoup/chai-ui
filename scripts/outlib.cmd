3828d4075ce2efb31f79c38795e3732bcc37f0b7
Garden <gardendistrict0x0@outlook.com>
2025-10-29 18:21:31 +1100

    [ refactor ] Turn off command outputs in outlib.cmd

--- scripts/outlib.cmd
+++ scripts/outlib.cmd
@@ -1,3 +1,7 @@
-cl /c /EHsc src\Chai\Inter\Display.cpp /Iinclude /Iinclude\Chai\Inter
-lib Display.obj /OUT:ChaiUI.lib
+@echo off
+echo Compiling sources...
+cl /c /EHsc src\Chai\Inter\Display.cpp /Iinclude /Iinclude\Chai\Inter >nul 2>&1
+echo Turning objects into library
+lib Display.obj /OUT:ChaiUI.lib >nul 2>&1
+echo Success!
 echo ChaiUI.lib is now located in the current directory
