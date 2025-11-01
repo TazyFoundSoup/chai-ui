9367175a6ee63a5181ab3206984caf41a912d7b2
Garden <gardendistrict0x0@outlook.com>
2025-11-01 19:55:21 +1100

    [ minor ] Add ... to end of library compilation for formatting

--- scripts/outlib.cmd
+++ scripts/outlib.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo Compiling sources...
 cl /c /EHsc src\Chai\Inter\Display.cpp /Iinclude /Iinclude\Chai\Inter >nul 2>&1
-echo Turning objects into library
+echo Turning objects into library...
 lib Display.obj /OUT:ChaiUI.lib >nul 2>&1
 echo Success!
 echo ChaiUI.lib is now located in the current directory
